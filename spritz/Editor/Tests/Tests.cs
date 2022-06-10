06d838f25cd0485b0129888fbecdd76ce5a38d6e
Sebastien Phaneuf <sebastienp@unity3d.com>
2022-06-10 15:43:57 -0400

    Add Rectutils for quick layout computation

---
+++ spritz/Editor/Tests/Tests.cs
@@ -0,0 +1,257 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UniMini;
+
+public class Tests
+{
+    [Test]
+    public void RectCutTests()
+    {
+        {
+            var r = new RectInt(2, 3, 4, 6);
+            var r1 = r.CutLeft(1);
+            Assert.AreEqual(new RectInt(3, 3, 3, 6), r);
+            Assert.AreEqual(new RectInt(2, 3, 1, 6), r1);
+        }
+        {
+            var r = new RectInt(2, 3, 4, 6);
+            var r1 = r.CutLeft(5);
+            Assert.AreEqual(new RectInt(6, 3, 0, 6), r);
+            Assert.AreEqual(new RectInt(2, 3, 4, 6), r1);
+        }
+        {
+            var r = new RectInt(2, 3, 4, 6);
+            var r1 = r.CutRight(1);
+            Assert.AreEqual(new RectInt(2, 3, 3, 6), r);
+            Assert.AreEqual(new RectInt(5, 3, 1, 6), r1);
+        }
+        {
+            var r = new RectInt(2, 3, 4, 6);
+            var r1 = r.CutRight(5);
+            Assert.AreEqual(new RectInt(2, 3, 0, 6), r);
+            Assert.AreEqual(new RectInt(2, 3, 4, 6), r1);
+        }
+
+        {
+            var r = new RectInt(2, 3, 4, 6);
+            var r1 = r.CutTop(1);
+            Assert.AreEqual(new RectInt(2, 3, 4, 5), r);
+            Assert.AreEqual(new RectInt(2, 8, 4, 1), r1);
+        }
+        {
+            var r = new RectInt(2, 3, 4, 6);
+            var r1 = r.CutTop(7);
+            Assert.AreEqual(new RectInt(2, 3, 4, 0), r);
+            Assert.AreEqual(new RectInt(2, 3, 4, 6), r1);
+        }
+
+        {
+            var r = new RectInt(2, 3, 4, 6);
+            var r1 = r.CutBottom(1);
+            Assert.AreEqual(new RectInt(2, 4, 4, 5), r);
+            Assert.AreEqual(new RectInt(2, 3, 4, 1), r1);
+        }
+        {
+            var r = new RectInt(2, 3, 4, 6);
+            var r1 = r.CutBottom(7);
+            Assert.AreEqual(new RectInt(2, 9, 4, 0), r);
+            Assert.AreEqual(new RectInt(2, 3, 4, 6), r1);
+        }
+    }
+
+    [Test]
+    public void RectGetTests()
+    {
+        {
+            var r = new RectInt(2, 3, 4, 6);
+            var r1 = r.GetLeft(1);
+            Assert.AreEqual(new RectInt(2, 3, 4, 6), r);
+            Assert.AreEqual(new RectInt(2, 3, 1, 6), r1);
+        }
+        {
+            var r = new RectInt(2, 3, 4, 6);
+            var r1 = r.GetLeft(5);
+            Assert.AreEqual(new RectInt(2, 3, 4, 6), r);
+            Assert.AreEqual(new RectInt(2, 3, 4, 6), r1);
+        }
+        {
+            var r = new RectInt(2, 3, 4, 6);
+            var r1 = r.GetRight(1);
+            Assert.AreEqual(new RectInt(2, 3, 4, 6), r);
+            Assert.AreEqual(new RectInt(5, 3, 1, 6), r1);
+        }
+        {
+            var r = new RectInt(2, 3, 4, 6);
+            var r1 = r.GetRight(5);
+            Assert.AreEqual(new RectInt(2, 3, 4, 6), r);
+            Assert.AreEqual(new RectInt(2, 3, 4, 6), r1);
+        }
+
+        {
+            var r = new RectInt(2, 3, 4, 6);
+            var r1 = r.GetTop(1);
+            Assert.AreEqual(new RectInt(2, 3, 4, 6), r);
+            Assert.AreEqual(new RectInt(2, 8, 4, 1), r1);
+        }
+        {
+            var r = new RectInt(2, 3, 4, 6);
+            var r1 = r.GetTop(7);
+            Assert.AreEqual(new RectInt(2, 3, 4, 6), r);
+            Assert.AreEqual(new RectInt(2, 3, 4, 6), r1);
+        }
+
+        {
+            var r = new RectInt(2, 3, 4, 6);
+            var r1 = r.GetBottom(1);
+            Assert.AreEqual(new RectInt(2, 3, 4, 6), r);
+            Assert.AreEqual(new RectInt(2, 3, 4, 1), r1);
+        }
+        {
+            var r = new RectInt(2, 3, 4, 6);
+            var r1 = r.GetBottom(7);
+            Assert.AreEqual(new RectInt(2, 3, 4, 6), r);
+            Assert.AreEqual(new RectInt(2, 3, 4, 6), r1);
+        }
+    }
+
+    [Test]
+    public void RectAddTests()
+    {
+        {
+            var r = new RectInt(2, 3, 4, 6);
+            var r1 = r.AddLeft(1);
+            Assert.AreEqual(new RectInt(2, 3, 4, 6), r);
+            Assert.AreEqual(new RectInt(1, 3, 5, 6), r1);
+        }
+
+        {
+            var r = new RectInt(2, 3, 4, 6);
+            var r1 = r.AddRight(1);
+            Assert.AreEqual(new RectInt(2, 3, 4, 6), r);
+            Assert.AreEqual(new RectInt(2, 3, 5, 6), r1);
+        }
+
+        {
+            var r = new RectInt(2, 3, 4, 6);
+            var r1 = r.AddTop(1);
+            Assert.AreEqual(new RectInt(2, 3, 4, 6), r);
+            Assert.AreEqual(new RectInt(2, 3, 4, 7), r1);
+        }
+
+        {
+            var r = new RectInt(2, 3, 4, 6);
+            var r1 = r.AddBottom(1);
+            Assert.AreEqual(new RectInt(2, 3, 4, 6), r);
+            Assert.AreEqual(new RectInt(2, 2, 4, 7), r1);
+        }
+    }
+
+    [Test]
+    public void RectUtilityTests()
+    {
+        {
+            var r = new RectInt(2, 3, 4, 6);
+            var r1 = r.Extend(1);
+            Assert.AreEqual(new RectInt(2, 3, 4, 6), r);
+            Assert.AreEqual(new RectInt(1, 2, 6, 8), r1);
+        }
+
+        {
+            var r = new RectInt(2, 3, 4, 6);
+            var r1 = r.Contract(1);
+            Assert.AreEqual(new RectInt(2, 3, 4, 6), r);
+            Assert.AreEqual(new RectInt(3, 4, 2, 4), r1);
+        }
+
+        {
+            var r = new RectInt(2, 3, 4, 6);
+            var r1 = r.Contract(7);
+            Assert.AreEqual(new RectInt(2, 3, 4, 6), r);
+            Assert.AreEqual(new RectInt(6, 9, 0, 0), r1);
+        }
+    }
+
+    [Test]
+    public void RectMarginTests()
+    {
+        {
+            var r = new RectInt(2, 3, 4, 6);
+            var r1 = r.AddMargin(1);
+            Assert.AreEqual(new RectInt(2, 3, 4, 6), r);
+            Assert.AreEqual(r.Extend(1), r1);
+            Assert.AreEqual(new RectInt(1, 2, 6, 8), r1);
+        }
+
+        {
+            var r = new RectInt(2, 3, 4, 6);
+            // vertical | horizontal
+            var r1 = r.AddMargin(1, 2);
+            Assert.AreEqual(new RectInt(2, 3, 4, 6), r);
+            Assert.AreEqual(new RectInt(0, 2, 8, 8), r1);
+        }
+
+        {
+            var r = new RectInt(2, 3, 4, 6);
+            // top | horizontal | bottom
+            var r1 = r.AddMargin(1, 2, 3);
+            Assert.AreEqual(new RectInt(2, 3, 4, 6), r);
+            Assert.AreEqual(new RectInt(0, 0, 8, 10), r1);
+        }
+
+        {
+            var r = new RectInt(2, 3, 4, 6);
+            // top | right | bottom | left
+            var r1 = r.AddMargin(1, 2, 3, 4);
+            Assert.AreEqual(new RectInt(2, 3, 4, 6), r);
+            Assert.AreEqual(new RectInt(-2, 0, 10, 10), r1);
+        }
+    }
+
+    [Test]
+    public void RectPaddingTests()
+    {
+        {
+            var r = new RectInt(2, 3, 4, 6);
+            var r1 = r.AddPadding(1);
+            Assert.AreEqual(new RectInt(2, 3, 4, 6), r);
+            Assert.AreEqual(r.Contract(1), r1);
+            Assert.AreEqual(new RectInt(3, 4, 2, 4), r1);
+        }
+
+        {
+            var r = new RectInt(2, 3, 4, 6);
+            // vertical | horizontal
+            var r1 = r.AddPadding(1, 2);
+            Assert.AreEqual(new RectInt(2, 3, 4, 6), r);
+            Assert.AreEqual(new RectInt(4, 4, 0, 4), r1);
+        }
+
+        {
+            var r = new RectInt(2, 3, 14, 16);
+            // vertical | horizontal
+            var r1 = r.AddPadding(1, 2);
+            Assert.AreEqual(new RectInt(2, 3, 14, 16), r);
+            Assert.AreEqual(new RectInt(4, 4, 10, 14), r1);
+        }
+
+        {
+            var r = new RectInt(2, 3, 14, 16);
+            // top | horizontal | bottom
+            var r1 = r.AddPadding(1, 2, 3);
+            Assert.AreEqual(new RectInt(2, 3, 14, 16), r);
+            Assert.AreEqual(new RectInt(4, 6, 10, 12), r1);
+        }
+
+        {
+            var r = new RectInt(2, 3, 14, 16);
+            // top | right | bottom | left
+            var r1 = r.AddPadding(1, 2, 3, 4);
+            Assert.AreEqual(new RectInt(2, 3, 14, 16), r);
+            Assert.AreEqual(new RectInt(6, 6, 8, 12), r1);
+        }
+    }
+
+}
---
+++ spritz/Runtime/RectUtil.cs
@@ -0,0 +1,159 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UniMini
+{
+    public static class RectUtil
+    {
+        public static RectInt CutLeft(this ref RectInt r, int v)
+        {
+            var minX = r.xMin;
+            r.xMin = Mathf.Min(r.xMax, r.xMin + v);
+            return new RectInt(minX, r.yMin, r.xMin - minX, r.height);
+        }
+
+        public static RectInt CutRight(this ref RectInt r, int v)
+        {
+            var maxX = r.xMax;
+            r.xMax = Mathf.Max(r.xMin, r.xMax - v);
+            return new RectInt(r.xMax, r.yMin, maxX - r.xMax, r.height);
+        }
+
+        public static RectInt CutTop(this ref RectInt r, int v)
+        {
+            var maxY = r.yMax;
+            r.yMax = Mathf.Max(r.yMin, r.yMax - v);
+            return new RectInt(r.xMin, r.yMax, r.width, maxY - r.yMax);
+        }
+
+        public static RectInt CutBottom(this ref RectInt r, int v)
+        {
+            var minY = r.yMin;
+            r.yMin = Mathf.Min(r.yMin + v, r.yMax);
+            return new RectInt(r.xMin, minY, r.width, r.yMin - minY);
+        }
+
+        public static RectInt GetLeft(this RectInt r, int v)
+        {
+            v = Mathf.Min(r.width, v);
+            return new RectInt(r.xMin, r.yMin, v, r.height);
+        }
+
+        public static RectInt GetRight(this RectInt r, int v)
+        {
+            v = Mathf.Min(r.width, v);
+            return new RectInt(r.xMax - v, r.yMin, v, r.height);
+        }
+
+        public static RectInt GetTop(this RectInt r, int v)
+        {
+            v = Mathf.Min(r.height, v);
+            return new RectInt(r.xMin, r.yMax - v, r.width, v);
+        }
+
+        public static RectInt GetBottom(this RectInt r, int v)
+        {
+            v = Mathf.Min(r.height, v);
+            return new RectInt(r.xMin, r.yMin, r.width, v);
+        }
+
+        public static RectInt AddLeft(this RectInt r, int v)
+        {
+            return new RectInt(r.xMin - v, r.yMin, r.width + v, r.height);
+        }
+
+        public static RectInt AddRight(this RectInt r, int v)
+        {
+            return new RectInt(r.xMin, r.yMin, r.width + v, r.height);
+        }
+
+        public static RectInt AddTop(this RectInt r, int v)
+        {
+            return new RectInt(r.xMin, r.yMin, r.width, r.height + v);
+        }
+
+        public static RectInt AddBottom(this RectInt r, int v)
+        {
+            return new RectInt(r.xMin, r.yMin - v, r.width, r.height + v);
+        }
+
+        public static RectInt Extend(this RectInt r, int v)
+        {
+            return new RectInt(r.xMin - v, r.yMin - v, r.width + 2*v, r.height + 2*v);
+        }
+
+        public static RectInt Contract(this RectInt r, int v)
+        {
+            var xMin = Mathf.Min(r.xMin + v, r.xMax);
+            var xMax = Mathf.Max(r.xMin, r.xMax - v);
+            var yMin = Mathf.Min(r.yMin + v, r.yMax);
+            var yMax = Mathf.Max(r.yMin, r.yMax - v);
+            return new RectInt(xMin, yMin, Mathf.Max(xMax - xMin, 0), Mathf.Max(yMax - yMin, 0));
+        }
+
+        /*
+            v1: all sides
+            v1 v2 : vertical | horizontal
+            v1 v2 v3 : top | horizontal | bottom
+            v1 v2 v3 v4: top | right | bottom | left
+         */
+        public static RectInt AddMargin(this RectInt r, int v)
+        {
+            return r.Extend(v);
+        }
+
+        public static RectInt AddMargin(this RectInt r, int v, int h)
+        {
+            return new RectInt(r.xMin - h, r.yMin - v, r.width + 2 * h, r.height + 2 * v);
+        }
+
+        public static RectInt AddMargin(this RectInt r, int t, int h, int b)
+        {
+            return new RectInt(r.xMin - h, r.yMin - b, r.width + 2 * h, r.height + t + b);
+        }
+
+        public static RectInt AddMargin(this RectInt r, int t, int right, int b, int l)
+        {
+            return new RectInt(r.xMin - l, r.yMin - b, r.width + right + l, r.height + t + b);
+        }
+
+        /*
+            v1: all sides
+            v1 v2 : vertical | horizontal
+            v1 v2 v3 : top | horizontal | bottom
+            v1 v2 v3 v4: top | right | bottom | left
+         */
+        public static RectInt AddPadding(this RectInt r, int v)
+        {
+            return r.Contract(v);
+        }
+
+        public static RectInt AddPadding(this RectInt r, int v, int h)
+        {
+            var xMin = Mathf.Min(r.xMin + h, r.xMax);
+            var xMax = Mathf.Max(r.xMin, r.xMax - h);
+            var yMin = Mathf.Min(r.yMin + v, r.yMax);
+            var yMax = Mathf.Max(r.yMin, r.yMax - v);
+            return new RectInt(xMin, yMin, Mathf.Max(xMax - xMin, 0), Mathf.Max(yMax - yMin, 0));
+        }
+
+        public static RectInt AddPadding(this RectInt r, int t, int h, int b)
+        {
+            var xMin = Mathf.Min(r.xMin + h, r.xMax);
+            var xMax = Mathf.Max(r.xMin, r.xMax - h);
+            var yMin = Mathf.Min(r.yMin + b, r.yMax);
+            var yMax = Mathf.Max(r.yMin, r.yMax - t);
+            return new RectInt(xMin, yMin, Mathf.Max(xMax - xMin, 0), Mathf.Max(yMax - yMin, 0));
+        }
+
+        public static RectInt AddPadding(this RectInt r, int t, int right, int b, int l)
+        {
+            var xMin = Mathf.Min(r.xMin + l, r.xMax);
+            var xMax = Mathf.Max(r.xMin, r.xMax - right);
+            var yMin = Mathf.Min(r.yMin + b, r.yMax);
+            var yMax = Mathf.Max(r.yMin, r.yMax - t);
+            return new RectInt(xMin, yMin, Mathf.Max(xMax - xMin, 0), Mathf.Max(yMax - yMin, 0));
+        }
+    }
+}
